39d1a1faabd0202aa5dfb19b45b57912a33edcf8
doogoodooogoo <deucalion4025@gmail.com>
2026-04-26 23:09:09 +0900

    add travel rating

--- src/App.tsx
+++ src/App.tsx
@@ -23,23 +23,55 @@ function App() {
     fetchWeather();
   }, []);
 
+  const getTravelScore = (weather: any) => {
+    let score = 0;
+
+    const condition = weather.weather[0].main;
+    const temp = weather.main.temp;
+
+    // 날씨 조건
+    if (condition === "Clear") score += 40;
+    else if (condition === "Clouds") score += 10;
+    else if (condition === "Rain") score -= 30;
+    else score += 0;
+
+    // 온도 조건
+    if (temp >= 15 && temp <= 25) score += 30;
+    else score -= 20;
+
+    return score;
+  };
+
+  const getMessage = (score: number) => {
+  if (score >= 60) return "여행하기 최고 👍";
+  if (score >= 30) return "무난 👍";
+  return "오늘은 비추천 😢";
+  };
+
   return (
     <div>
       <h1>Japan Weather</h1>
 
-      {weatherList.map((w, index) => (
-        <div key={index}>
-          <h2>{w.name}</h2>
+      {weatherList.map((w, index) => {
+        const score = getTravelScore(w);
 
-          <img
-            src={`https://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png`}
-            alt="weather icon"
-          />
+        return (
+          <div key={index}>
+            <h2>{w.name}</h2>
 
-          <p>온도: {w.main.temp}°C</p>
-          <p>날씨: {w.weather[0].main}</p>
-        </div>
-      ))}
+            <img
+              src={`https://openweathermap.org/img/wn/${w.weather[0].icon}@2x.png`}
+              alt="weather icon"
+            />
+
+            <p>온도: {w.main.temp}°C</p>
+            <p>날씨: {w.weather[0].main}</p>
+
+            <p>여행 점수: {score}점</p>
+            <p>{getMessage(score)}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
